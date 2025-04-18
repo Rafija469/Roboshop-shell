66736a30ce7897aa056cb331f0504d20a804f2f1
Rafija <you@example.com>
2025-04-18 20:26:52 +0530

    my project

--- cart.sh
+++ cart.sh
@@ -1,13 +1,7 @@
 componet_name = cart
 source common.sh
 nodejs
+
 cp cart.service /etc/systemd/system/cart.service
-useradd roboshop
-rm -rf /app
-mkdir /app
-curl -L -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart-v3.zip
-cd /app
-unzip /tmp/cart.zip
-cd /app
-npm install
+
 systemd
--- common.sh
+++ common.sh
@@ -7,4 +7,12 @@ nodejs(){
   dnf module disable nodejs -y
   dnf module enable nodejs:20 -y
   dnf install nodejs -y
+  useradd roboshop
+  rm -rf /app
+  mkdir /app
+  curl -o /tmp/component_name.zip https://roboshop-artifacts.s3.amazonaws.com/component_name-v3.zip
+  cd /app
+  unzip /tmp/component_name.zip
+  cd /app
+  npm install
 }
