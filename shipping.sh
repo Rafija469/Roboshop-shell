7dc3c2871c5211c910a5b652ab5563272a6b2ea4
Rafija <you@example.com>
2025-04-21 16:34:34 +0530

    my project

--- shipping.sh
+++ shipping.sh
@@ -1,19 +1,11 @@
-dnf install maven -y
-useradd roboshop
-rm -rf /app
-mkdir /app
-curl -L -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping-v3.zip
-cd /app
-unzip /tmp/shipping.zip
-cd /app
-mvn clean package
-mv target/shipping-1.0.jar shipping.jar
+component_name=shipping
+source common.sh
+
+java
 
 dnf install mysql -y
 mysql -h mysql-dev.azrdevops82.online -uroot -pRoboShop@1 < /app/db/schema.sql
 mysql -h mysql-dev.azrdevops82.online -uroot -pRoboShop@1 < /app/db/app-user.sql
 mysql -h mysql-dev.azrdevops82.online -uroot -pRoboShop@1 < /app/db/master-data.sql
 
-ystemctl daemon-reload
-systemctl enable shipping
-systemctl start shipping
+systemd
