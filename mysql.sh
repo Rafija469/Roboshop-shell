1c625f7f726af3e6d4db29b0e39c5ae88c487e9c
Rafija <you@example.com>
2025-04-21 17:30:28 +0530

    my project

--- mysql.sh
+++ mysql.sh
@@ -1,4 +1,18 @@
+if [ -z "${MYSQL_ROOT_PASSWORDT"} -O -z "$1" ]; then
+echo input password is missing
+exit
+fi
+
+if [ -n "${MYSQL_ROOT_PASSWORDT"} ]; then
+PASSWORD_$MYSQL_ROOT_PASSWORDT
+fi
+
+ if [ -n "$1" ]; then
+ PASSWORD=$1
+ fi
+
 dnf install mysql-server -y
 systemctl enable mysqld
 systemctl start mysqld
-mysql_secure_installation --set-root-pass RoboShop@1
+
+mysql_secure_installation --set-root-pass $PASSWORD
--- shipping.sh
+++ shipping.sh
@@ -1,11 +1,16 @@
+if [ -z "$1" ]; then
+  echo input password is missing
+  exit
+  fi
+
 component_name=shipping
 source common.sh
 
 java
 
 dnf install mysql -y
-mysql -h mysql-dev.azrdevops82.online -uroot -pRoboShop@1 < /app/db/schema.sql
-mysql -h mysql-dev.azrdevops82.online -uroot -pRoboShop@1 < /app/db/app-user.sql
-mysql -h mysql-dev.azrdevops82.online -uroot -pRoboShop@1 < /app/db/master-data.sql
+for file in schema app-user master-data1 do
+mysql -h mysql-dev.azrdevops82.online -uroot -p$1 < /app/db/$file.sql
+done
 
 systemd
